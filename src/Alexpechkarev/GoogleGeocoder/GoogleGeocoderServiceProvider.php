d2abfe29120bf2d43afcb58f4104c85bbdd4b061
Alexander Pechkarev <alexpechkarev@gmail.com>
2014-07-12 19:18:54 +0100

    Init repository

---
+++ src/Alexpechkarev/GoogleGeocoder/GoogleGeocoderServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Alexpechkarev\GoogleGeocoder;
+
+use Illuminate\Support\ServiceProvider;
+
+class GoogleGeocoderServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}

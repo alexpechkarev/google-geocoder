c935b60eadc69d5713bdea3988e449612fbbc876
Martin Lindhe <martin@ubique.se>
2015-02-06 16:34:46 +0100

    Update for Laravel 5 configuration, now config file live in config/google-geocoder.php

--- src/Alexpechkarev/GoogleGeocoder/GoogleGeocoderServiceProvider.php
+++ src/Alexpechkarev/GoogleGeocoder/GoogleGeocoderServiceProvider.php
@@ -8,63 +8,60 @@ use Illuminate\Support\Facades\Config;
 
 class GoogleGeocoderServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
 
-        /**
-         * Bootstrap the application events.
-         *
-         * @return void
-         */
-        public function boot()
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        AliasLoader::getInstance()->alias('Geocoder','Alexpechkarev\GoogleGeocoder\Facades\GoogleGeocoderFacade');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['GoogleGeocoder'] = $this->app->share(function($app)
         {
-            AliasLoader::getInstance()->alias('Geocoder','Alexpechkarev\GoogleGeocoder\Facades\GoogleGeocoderFacade');
-        }
+            $config = array();
+            $config['applicationKey']   = Config::get('google-geocoder.applicationKey');
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
+            // Throw an error if application key is empty
+            if (empty($config['applicationKey'])) {
+                throw new \InvalidArgumentException('Application Key is empty, please check your config file.');
+            }
 
-            $this->app['GoogleGeocoder'] = $this->app->share(function($app)
-            {
-                $config = array();
-                $config['applicationKey']   = Config::get('google-geocoder::applicationKey');
+            $config['requestUrl']       = Config::get('google-geocoder.requestUrl');
 
-                // Throw an error if application key is empty
-                if(empty($config['applicationKey'])):
-                    throw new \InvalidArgumentException('Application Key is empty, please check your config file.');
-                endif;
+            // Throw an error if request URL is empty
+            if (empty($config['requestUrl'])) {
+                throw new \InvalidArgumentException('Request URL is empty, please check your config file.');
+            }
 
-                $config['requestUrl']       = Config::get('google-geocoder::requestUrl');
+            return new GoogleGeocoder($config);
+        });
+    }
 
-                // Throw an error if request URL is empty
-                if(empty($config['requestUrl'])):
-                    throw new \InvalidArgumentException('Request URL is empty, please check your config file.');
-                endif;
-
-
-                return new GoogleGeocoder($config);
-
-            });
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
